9f5ae54f9f884181b676901cf884385a614c309b
Mason Wray <masonwray.mw@gmail.com>
2019-09-29 08:37:42 -0500

    Delete index.js

--- dev/index.js
+++
@@ -1,7 +0,0 @@
-// Mason Wray - 2019
-
-// definitions
-let $ = require('jquery');
-const { dialog } = require('electron').remote;
-let data = require('nedb');
-let paths = new data({ filename: "pages/import/paths", autoload: true });
